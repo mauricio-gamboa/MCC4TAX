af7e6d205acd0611aa152970b320ef5c90430553
Mauricio Gamboa <magalocr@gmail.com>
2015-08-06 08:49:16 -0600

    Feedback

--- template_parts/content-logos.php
+++ template_parts/content-logos.php
@@ -1,4 +1,6 @@
 <?php $images = explode( ',', ot_get_option( 'logos', '' ) ); ?>
+<?php $columns = count($images); ?>
+<?php $width = 100 / $columns; ?>
 
 <section id="as-seen-on" class="site-padding-bottom">
   <div class="container">
@@ -9,7 +11,7 @@
         <div class="aso-wrapper clearfix">
           <?php foreach( $images as $id ): if ( ! empty( $id ) ): ?>
           <?php $full_img_src = wp_get_attachment_image_src( $id, '' ); ?>
-          <div class="aso"><img src="<?php echo $full_img_src[0];?>" alt=""/></div>
+          <div class="aso" style="width: <?php echo $width; ?>%;"><img src="<?php echo $full_img_src[0];?>" alt=""/></div>
           <?php endif; endforeach; ?>
         </div>
       </div>
